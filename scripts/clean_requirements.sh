cfc597256952b0be5bd02470658e2cac2d44183a
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-10-03 13:59:12 +0100

    update worflow

--- scripts/clean_requirements.sh
+++
@@ -1,2 +0,0 @@
-python -c "print(''.join([l.split(';')[0]+'\n' if 'python_version' in l else l for l in open('requirements.txt', 'r').readlines()]))" > requirements_tmp.txt
-mv requirements_tmp.txt requirements.txt
